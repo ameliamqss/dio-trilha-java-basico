3ba7c46d4b5300746e7430b4c58589f685034d8a
ameliamqss <ameliaamorais13@gmail.com>
2025-04-28 16:51:04 -0300

    Simulando Uma Conta Bancária Através Do Terminal

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         int numero;
         String agencia, nomeCliente;
         double saldo;
