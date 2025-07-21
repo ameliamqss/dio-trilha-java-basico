34466e0b12f901a82ef7f2e0788a4a0b6a886bdf
ameliamqss <ameliaamorais13@gmail.com>
2025-07-21 17:13:48 -0300

    OPERAÇÕES BÁSICAS COM LIST

--- TestesAulas/TestesDaAula/Tarefa.java
+++ TestesAulas/TestesDaAula/Tarefa.java
@@ -2,7 +2,7 @@ public class Tarefa {
     private String descricao;
 
     public Tarefa(String descricao){
-        this.decricao= descricao;
+        this.descricao= descricao;
     }
 
     public String getDescricao() {
